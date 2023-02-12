cc3bf3b8e6d1e6123162fdda1e24807b8c9e18b4
AAlpha7 <L@9329.me>
2023-02-11 20:21:01 -0800

    Updated to get better microphone input recogniztion and improve clarity of the output.

--- Code/VoiceChatGPT.py
+++ Code/VoiceChatGPT.py
@@ -5,22 +5,22 @@ DESCRIPTION:
 
 This code is an AI chatbot that uses Google Cloud Speech-to-Text, OpenAI's GPT-3, and Google Cloud Text-to-Speech to respond to a user's voice input.
 
-The recognize_microphone function records audio from the microphone for 5 seconds and then transcribes the audio to text using Google Cloud Speech-to-Text. The resulting transcript is returned.
+The transcribe_speech() function records audio from the microphone and then transcribes the audio to text using Google Cloud Speech-to-Text. The resulting transcript is returned.
 
-The OpenAI API key is loaded from a file using the get_key_from_file function and the conversation history is stored in the conversation string. The ask function uses the conversation history and OpenAI's GPT-3 to generate a response to the user's input.
+The OpenAI API key is loaded from a file using the openai_key_file_path variable and the conversation history is stored in the conversation string. The ask(newQuestion) function uses the conversation history and OpenAI's GPT-3 to generate a response to the user's input.
 
-The text_to_mp3 function takes a text string and generates an MP3 audio file using Google Cloud Text-to-Speech. The play_mp3 function plays an MP3 audio file using pygame.
+The text_to_mp3(text, voice_name, filename) function takes a text string, a voice name, and a filename and generates an MP3 audio file using Google Cloud Text-to-Speech. The play_mp3(filename) function plays an MP3 audio file using pygame.
 
 The main loop of the code repeatedly records audio from the microphone, generates a response from the AI chatbot, and plays the response as audio. The conversation history is updated after each iteration and displayed after the chat ends. The chat ends when the user says "结束对话" (end conversation).
 
-PLEASE NOTE: 
-    1. The code is programed to work with Chinese. To use the code with a different language, you will need to change the language code in the recognize_microphone function and the voice name in the text_to_mp3 function.
-        - Language codes: https://cloud.google.com/speech-to-text/docs/languages
-        - Voice names: https://cloud.google.com/text-to-speech/docs/voices
-    2. The command to end the conversasion is also Chinese ("结束对话"). To change the command, you will need to change the if statement in the main loop.
-    3. You will need to create a Google Cloud account and enable the Speech-to-Text, Text-to-Speech, and Translate APIs. You will also need to create an OpenAI account and get an API key.
-        - Google Cloud: https://cloud.google.com/
-        - OpenAI: https://openai.com/
+PLEASE NOTE:
+1. The code is programed to work with Chinese. To use the code with a different language, you will need to change the language code in the transcribe_speech() function and the voice name in the text_to_mp3() function.
+- Language codes: https://cloud.google.com/speech-to-text/docs/languages
+- Voice names: https://cloud.google.com/text-to-speech/docs/voices
+2. The command to end the conversasion is also Chinese ("结束对话"). To change the command, you will need to change the if statement in the main loop.
+3. You will need to create a Google Cloud account and enable the Speech-to-Text, Text-to-Speech, and Translate APIs. You will also need to create an OpenAI account and get an API key.
+- Google Cloud: https://cloud.google.com/
+- OpenAI: https://openai.com/
 
 ENJOY!
 """
@@ -62,64 +62,48 @@ with open(openai_key_file_path, 'r') as file:
 
 # ---- Define a function to recognize speech from the microphone ----
 
-def recognize_microphone(language_code='cmn-Hans-CN'):
-    """Transcribe speech from recorded from `microphone`."""
+import speech_recognition as sr
+import os
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import types
 
-    # Record Audio
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = "output.wav"
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    print(".")
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("!")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    # Transcribe the recorded audio
+def transcribe_speech():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        #print("请开始讲话...")
+        audio = r.listen(source, timeout=30)
+        #print("识别中，请稍等...")
+        
     client = speech_v1.SpeechClient()
-    with io.open(WAVE_OUTPUT_FILENAME, 'rb') as audio_file:
-        content = audio_file.read()
 
-    audio = speech_v1.types.RecognitionAudio(content=content)
-    config = speech_v1.types.RecognitionConfig(
+    config = types.RecognitionConfig(
         encoding=types.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code=language_code)
+        sample_rate_hertz=44100,
+        language_code='zh-CN'
+    )
 
-    response = client.recognize(config=config, audio=audio)
-    try:
-        result = response.results[0].alternatives[0].transcript
-    except IndexError:
-        result = ""
+    audio = types.RecognitionAudio(content=audio.get_wav_data())
 
-    return result
+    response = client.recognize(config = config, audio = audio)
+
+    # 遍历每一个识别结果，选择置信度最高的
+    best_transcript = ''
+    best_confidence = -1.0
+    for result in response.results:
+        transcript = result.alternatives[0].transcript
+        confidence = result.alternatives[0].confidence
+        #print('识别结果: {}'.format(transcript))
+        #print('置信度: {}'.format(confidence))
+        if confidence > best_confidence:
+            best_transcript = transcript
+            best_confidence = confidence
+    
+    # 如果没有检测到任何语音输入，则返回空字符串
+    if not best_transcript and r.energy_threshold < 400:
+        return ''
+    
+    return best_transcript
+
 
 
 # ---- Define a function to get a response from OpenAI GPT-3 ----
@@ -129,7 +113,7 @@ def ask(newQuestion):
         model="text-davinci-003",
         prompt=conversation,
         temperature=0.5,
-        max_tokens=500,
+        max_tokens=1000,
         top_p=0.3,
         frequency_penalty=0.5,
         presence_penalty=0,
@@ -174,7 +158,8 @@ conversation = "The following is a conversation with an AI assistant. The assist
 voiceText = ""
 tmpResponseVoiceFileName = f"answer.mp3"
 while "结束对话" not in voiceText:
-    voiceText = recognize_microphone()
+    #voiceText = recognize_microphone()
+    voiceText = transcribe_speech()
     if voiceText.strip() == "":
         continue
     elif "结束对话" in voiceText:
